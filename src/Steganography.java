8218c0f36210e13c205d055076ecfb3b69289ea7
Chris <csuh09@gmail.com>
2017-07-15 10:14:55 -0400

    removed license

--- src/Steganography.java
+++ src/Steganography.java
@@ -2,9 +2,24 @@
  *  Basic Steganography program to hide text or image data within
  *  the least significant red, green, and blue bits of images 
  *  supported by ImageIO.read
+ *  
+ *   Copyright (C) 2017  Christopher Suh
+ *   
+ *   This file is part of Steganography
  *
- *	@author Christopher Suh
- *	@date 19 May 2017
+ *   Steganography is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Steganography is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 import java.awt.Color;
